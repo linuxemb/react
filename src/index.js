b65fa03e19150286a353305886bee652bb50d03d
linuxemb <itss07@gmail.com>
2020-02-16 18:43:11 -0800

    study react from demo app

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+	console.log(action)
+	if (action.type === 'changeState')
+	{
+		return action.payload.newState;
+	}
+
+	return 'State1';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState())
+
+const action = {
+	type: `changeState`,
+	payload: {
+		newState: 'New state'
+	}
+};
+store.dispatch(action);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+/*function myapp() {
+
+	return {
+		<ul>
+		<li>1</li>
+		<li>2</li>
+		</ul>,
+		document.getElementById("root")
+
+	}
+
+}
+ReactDOM.render( document.getElementById("root")
+
+) */
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
