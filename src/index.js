a9facffdd55ba089d50a0438c9f4aa0727cac08b
linuxemb <itss07@gmail.com>
2020-02-17 18:29:14 -0800

    Reducer, store, dispatcher simple case

--- src/index.js
+++ src/index.js
@@ -2,31 +2,83 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux';
 
-function reducer(state, action) {
-	console.log(action)
-	if (action.type === 'changeState')
-	{
-		return action.payload.newState;
-	}
 
-	return 'State1';
-}
 
-const store = createStore(reducer);
+//ACTION INCREASE
+const increment = () => {
+	return {
+		type:'INCREMENT'
 
-console.log(store.getState())
+	};
 
-const action = {
-	type: `changeState`,
-	payload: {
-		newState: 'New state'
+};
+
+const decrement = () => {
+	return {
+		type:'DECREMENT'
+	};
+};
+
+
+//REDUCER
+
+const counter = (state=0, action)=> {
+
+	switch(action.type) {
+		case 'INCREMENT':
+		return state +1;
+		case  'DECREMENT':
+		return state -1;
+		default:
+		return state;
 	}
 };
-store.dispatch(action);
+
+
+//STORE 
+let store = createStore(counter)
+
+// display 
+store.subscribe(()=> console.log(store.getState()));
+
+
+
+//DISPATCH
+
+store.dispatch(increment())
+
+
+store.dispatch(increment())
+
+store.dispatch(increment())
+store.dispatch(decrement())
+
+
+// function reducer(state, action) {
+// 	console.log(action)
+// 	if (action.type === 'changeState')
+// 	{
+// 		return action.payload.newState;
+// 	}
+
+// 	return 'State1';
+// }
+
+// const store = createStore(reducer);
+
+// console.log(store.getState())
+
+// const action = {
+// 	type: `changeState`,
+// 	payload: {
+// 		newState: 'New state'
+// 	}
+// };
+// store.dispatch(action);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
@@ -50,4 +102,4 @@ ReactDOM.render( document.getElementById("root")
 // unregister() to register() below. Note this comes with some pitfalls.
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
